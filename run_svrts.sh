470757fe35f0fb51d828a3bfadb0914d4532c2f8
Drew Linsley <drewlinsley@gmail.com>
2016-03-30 20:05:59 -0400

    updated

--- run_svrts.sh
+++ run_svrts.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #for i in {1..20}
-#for i in 1 2 3 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
-$maindir="/home/ubuntu/draw_classify"
+#for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23
+maindir=/home/ubuntu/draw_classify
 for i in 1
 do
 	thisdata=$(printf "res_results_problem_%i" "$i")
